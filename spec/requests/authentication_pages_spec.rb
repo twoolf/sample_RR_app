f52303a4e9f5c720337f3d1bcfa0560072aa489c
twoolf <twoolf@jhu.edu>
2014-09-06 21:53:51 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,6 +22,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
       before do
         fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
@@ -30,6 +31,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       describe "followed by signout" do
