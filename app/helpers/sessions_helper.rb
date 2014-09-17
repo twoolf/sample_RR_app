53c311c2d83b2c9188b5806419195fe12ec624b1
twoolf <twoolf@jhu.edu>
2014-09-16 20:52:37 -0400

    more foods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
   def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
