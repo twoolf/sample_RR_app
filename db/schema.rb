2cda0de78171b696d53a87ca444d59e6a1871ef7
twoolf <twoolf@jhu.edu>
2014-09-20 08:48:50 -0400

    working followers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917005749) do
+ActiveRecord::Schema.define(version: 20140920001421) do
 
   create_table "eats", force: true do |t|
     t.string   "food"
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(version: 20140917005749) do
     t.datetime "updated_at"
     t.integer  "calories"
   end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
